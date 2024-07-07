968505de8b47a4bb644cd9d7bd65d80ab7cbccfb
Keshav Kumar <102866232+keshavkumar23@users.noreply.github.com>
2024-07-08 01:24:57 +0530

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import UserContextProvider from "./UserContext";
 import Routes from "./Routes";
 function App() {
-  axios.defaults.baseURL = "http://localhost:4040";
+  axios.defaults.baseURL = "https://student-feedback-system-backend.onrender.com";
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
