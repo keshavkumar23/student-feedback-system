8a3ac08cdca53e4f1884ea66762d371681935c51
Keshav Kumar <102866232+keshavkumar23@users.noreply.github.com>
2024-07-08 01:22:10 +0530

    studentRoutes.js

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const studentController = require('../controllers/StudentControllers');
+const studentController = require('../controllers/studentControllers');
 
 // Routes
 router.post('/register', studentController.registerStudent);
